8f507c61506c72c28036755eccfd3f5d1ecc0a26
Ariana Maghsoudi <ariana.maghsoudi82@gmail.com>
2025-08-30 12:20:40 +0330

    feat: Introduce tree utils

---
+++ Python/utils/tree.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+from typing import Any
+
+class TreeNode:
+    def __init__(self, val: Any, left:TreeNode|None=None, right:TreeNode|None=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    @classmethod
+    def from_list(cls, nodes: list[Any]) -> TreeNode|None:
+        if 0 == len(nodes):
+            return None
+        root: TreeNode = TreeNode(nodes[0])
+        current_depth: list[TreeNode] = [root]
+        next_depth: list[TreeNode] = []
+        i: int = 1
+        while len(nodes) > i:
+            for node in current_depth:
+                if nodes[i] is not None:
+                    node.left = TreeNode(nodes[i])
+                    next_depth.append(node.left)
+                if len(nodes) > i + 1 and nodes[i + 1] is not None:
+                    node.right = TreeNode(nodes[i + 1])
+                    next_depth.append(node.right)
+                i += 2
+            current_depth = next_depth
+            next_depth = []
+        return root
+
+    def to_list(self) -> list[Any]:
+        nodes: list[Any] = [self.val]
+        current_depth: list[TreeNode] = [self]
+        next_depth: list[TreeNode] = []
+        while 0 != len(current_depth):
+            for node in current_depth:
+                if node.left is not None:
+                    nodes.append(node.left.val)
+                    next_depth.append(node.left)
+                else:
+                    nodes.append(None)
+                if node.right is not None:
+                    nodes.append(node.right.val)
+                    next_depth.append(node.right)
+                else:
+                    nodes.append(None)
+            current_depth = next_depth
+            next_depth = []
+        i: int = len(nodes) - 1
+        while nodes[i] is None:
+            i -= 1
+        return nodes[0:i+1]
+
+    def __repr__(self):
+        return str(self.to_list())
